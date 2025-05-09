380f5ab9f6797265cc6682b86210848fef836da5
Nicolle <Marianicolledasilva@gmail.com>
2025-05-09 14:07:34 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
 import 'splash_screen.dart';
 
-// Função principal que inicia a aplicação
 void main() {
   runApp(const MaterialApp(
     home: SplashScreenWidget(),
@@ -10,7 +9,6 @@ void main() {
   ));
 }
 
-// Classe que DEFINE o estado da calculadora
 class Calculadora extends StatefulWidget {
   const Calculadora({super.key});
 
@@ -18,25 +16,22 @@ class Calculadora extends StatefulWidget {
   _CalculadoraState createState() => _CalculadoraState();
 }
 
-// Classe que GERENCIA o estado da calculadora
 class _CalculadoraState extends State<Calculadora> {
-  String _display = ''; // Variável que armazena o que está sendo exibido na tela
+  String _display = '';
 
-  // Função que atualiza o display com um novo valor
   void _updateDisplay(String value) {
     setState(() {
       _display += value;
     });
   }
 
-  // Função que mostra o resultado da expressão matemática
   void _showResult() {
     try {
-      final result = eval(_display); // Chama a função eval para calcular o resultado
+      final result = eval(_display);
       setState(() {
-        _display = result.toString(); // Atualiza o display com o resultado
+        _display = result.toString();
       });
-    } catch (e) { // Se houver um erro ao calcular o resultado exibe um alerta
+    } catch (e) {
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
@@ -55,36 +50,36 @@ class _CalculadoraState extends State<Calculadora> {
     }
   }
 
-  // Função que avalia uma expressão matemática e retorna o resultado
   double eval(String expression) {
-    expression = expression.replaceAll('×', '*').replaceAll('÷', '/'); // Substitui os símbolos de multiplicação e divisão
-    Parser p = Parser(); // Cria um parser para analisar a expressão
-    Expression exp = p.parse(expression); // Analisa a expressão e cria uma expressão matemática
-    final cm = ContextModel(); // Cria um modelo de contexto com incógnitas (vazio pq só trabalhamos com números)
-    return exp.evaluate(EvaluationType.REAL, cm); // Avalia a expressão e retorna o resultado
+    expression = expression.replaceAll('×', '*').replaceAll('÷', '/');
+    Parser p = Parser();
+    Expression exp = p.parse(expression);
+    final cm = ContextModel();
+    return exp.evaluate(EvaluationType.REAL, cm);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey,
+        backgroundColor: Colors.black, // fundo preto
         appBar: AppBar(
-          backgroundColor: Colors.deepPurpleAccent,
-          title: const Text('Calculadora do Pedro e da Nicolle', style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-          leading: const Icon(Icons.calculate, color: Colors.white, size: 30,),
+          backgroundColor: Colors.black, // appbar preta
+          title: const Text(
+            'Calculadora do Pedro e da Nicolle',
+            style: TextStyle(color: Colors.white, fontSize: 20),
           ),
-        
+          leading: const Icon(Icons.calculate, color: Colors.white, size: 30),
+        ),
         body: Column(
           children: [
-            // Display
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text(_display, style: TextStyle(fontSize: 30)),
+              child: Text(
+                _display,
+                style: const TextStyle(fontSize: 30, color: Colors.white), // texto branco
+              ),
             ),
-
-            // Botões
             Expanded(
               child: GridView(
                 padding: const EdgeInsets.all(8.0),
@@ -94,43 +89,60 @@ class _CalculadoraState extends State<Calculadora> {
                   crossAxisSpacing: 4.0,
                 ),
                 children: [
-                  // Linha 1
-                  ElevatedButton(onPressed: () => _updateDisplay('7'), child: const Text('7', style: TextStyle(fontSize: 30),)),
-                  ElevatedButton(onPressed: () => _updateDisplay('8'), child: const Text('8', style: TextStyle(fontSize: 30),)),
-                  ElevatedButton(onPressed: () => _updateDisplay('9'), child: const Text('9', style: TextStyle(fontSize: 30),)),
-                  ElevatedButton(onPressed: () => _updateDisplay('÷'), child: const Text('÷', style: TextStyle(fontSize: 60),)),
-
-                  // Linha 2
-                  ElevatedButton(onPressed: () => _updateDisplay('4'), child: const Text('4', style: TextStyle(fontSize: 30),)),
-                  ElevatedButton(onPressed: () => _updateDisplay('5'), child: const Text('5', style: TextStyle(fontSize: 30),)),
-                  ElevatedButton(onPressed: () => _updateDisplay('6'), child: const Text('6', style: TextStyle(fontSize: 30),)),
-                  ElevatedButton(onPressed: () => _updateDisplay('×'), child: const Text('×', style: TextStyle(fontSize: 60),)),
-
-                  // Linha 3
-                  ElevatedButton(onPressed: () => _updateDisplay('1'), child: const Text('1', style: TextStyle(fontSize: 30),)),
-                  ElevatedButton(onPressed: () => _updateDisplay('2'), child: const Text('2', style: TextStyle(fontSize: 30),)),
-                  ElevatedButton(onPressed: () => _updateDisplay('3'), child: const Text('3', style: TextStyle(fontSize: 30),)),
-                  ElevatedButton(onPressed: () => _updateDisplay('-'), child: const Text('-', style: TextStyle(fontSize: 60),)),
-
-                  // Linha 4
-                  ElevatedButton(onPressed: () => _showResult(), child: const Text('=', style: TextStyle(fontSize: 60),)),
-                  ElevatedButton(onPressed: () => _updateDisplay('0'), child: const Text('0', style: TextStyle(fontSize: 30),)),
-                  ElevatedButton(onPressed: () => _updateDisplay('.'), child: const Text('.', style: TextStyle(fontSize: 60),)),
-                  ElevatedButton(onPressed: () => _updateDisplay('+'), child: const Text('+', style: TextStyle(fontSize: 60),)),
-
-                  // Limpar resultado ou display
+                  // Botões
+                  buildButton('7'),
+                  buildButton('8'),
+                  buildButton('9'),
+                  buildButton('÷', operator: true),
+                  buildButton('4'),
+                  buildButton('5'),
+                  buildButton('6'),
+                  buildButton('×', operator: true),
+                  buildButton('1'),
+                  buildButton('2'),
+                  buildButton('3'),
+                  buildButton('-', operator: true),
+                  ElevatedButton(
+                    onPressed: _showResult,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey[850],
+                    ),
+                    child: const Text('=', style: TextStyle(fontSize: 60, color: Colors.white)),
+                  ),
+                  buildButton('0'),
+                  buildButton('.', operator: true),
+                  buildButton('+', operator: true),
                   ElevatedButton(
                     onPressed: () {
                       setState(() {
                         _display = '';
                       });
                     },
-                    child: const Text('C', style: TextStyle(fontSize: 30),),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red[900],
+                    ),
+                    child: const Text('C', style: TextStyle(fontSize: 30, color: Colors.white)),
                   ),
                 ],
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildButton(String text, {bool operator = false}) {
+    return ElevatedButton(
+      onPressed: () => _updateDisplay(text),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: operator ? Colors.grey[800] : Colors.grey[900],
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: text == '÷' || text == '×' || text == '-' || text == '+' || text == '=' || text == '.' ? 60 : 30,
+          color: Colors.white,
         ),
       ),
     );
